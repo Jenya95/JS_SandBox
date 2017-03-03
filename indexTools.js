3c79e8e9430ad7d0daf78ef4940582f1f84db53e
Jenya95 <sanevichj@gmail.com>
2017-03-03 13:39:04 +0300

    Initial commit

---
+++ indexTools.js
@@ -0,0 +1,45 @@
+/**
+ * Created by esanevich on 03.03.2017.
+ */
+$(document).ready(function () {
+    var input = $("#recInput");
+    var action = true;
+
+    input.keydown(function(eventObject){
+        if ((eventObject.which == 17)&&(action === true)) {
+            action = false;
+            console.log("control");
+            recognize();
+        }
+    });
+
+    input.keyup(function(eventObject){
+        if (eventObject.which == 17) {
+            action = true;
+        }
+    });
+
+    function recognize() {
+        window.ya.speechkit.settings.apikey = 'c4936434-79dd-4fd4-9edf-86c25699c967';
+        ya.speechkit.recognize({
+            // Функция будет вызвана, когда распознавание завершится.
+            doneCallback: function(text) {
+                input.val(text);
+                console.log("Финальный результат распознавания: " + text);
+            },
+            // Функция вызовется, как только сессия будет инициализирована.
+            initCallback: function () {
+                input.focus();
+                console.log("Процесс распознавания запущен.");
+            },
+            // Вызывается в случае возникновения ошибки.
+            errorCallback: function(err) {
+                console.log("Возникла ошибка: " + err);
+            },
+            // Длительность промежутка тишины, при наступлении которой
+            // распознавание завершается.
+            utteranceSilence: 5
+        });
+    }
+
+});
